9fcd7daa21122ef4cf905dd33b45a31f78919850
tunglam20895 <tunglam20895@gmail.com>
2021-07-20 01:23:45 +0700

    fix

--- src/app/views/admin/project/project.module.ts
+++ src/app/views/admin/project/project.module.ts
@@ -1,9 +1,9 @@
 import { ProjectResComponent } from './project-detail/project-res/project-res.component';
 import { ProjectDasboardComponent } from './project-detail/project-dasboard/project-dasboard.component';
-import { CardSocialTrafficComponent } from './../../../../../../ITsolTest/src/app/components/cards/card-social-traffic/card-social-traffic.component';
-import { CardPageVisitsComponent } from './../../../../../../ITsolTest/src/app/components/cards/card-page-visits/card-page-visits.component';
-import { CardBarChartComponent } from './../../../../../../ITsolTest/src/app/components/cards/card-bar-chart/card-bar-chart.component';
-import { CardLineChartComponent } from './../../../../../../ITsolTest/src/app/components/cards/card-line-chart/card-line-chart.component';
+import { CardSocialTrafficComponent } from './../../../../../../MP-FrontEnd/src/app/components/cards/card-social-traffic/card-social-traffic.component';
+import { CardPageVisitsComponent } from './../../../../../../MP-FrontEnd/src/app/components/cards/card-page-visits/card-page-visits.component';
+import { CardBarChartComponent } from './../../../../../../MP-FrontEnd/src/app/components/cards/card-bar-chart/card-bar-chart.component';
+import { CardLineChartComponent } from './../../../../../../MP-FrontEnd/src/app/components/cards/card-line-chart/card-line-chart.component';
 import { ProjectTeamComponent } from './project-detail/project-team/project-team.component';
 import { MatTableModule } from '@angular/material/table';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
@@ -15,20 +15,17 @@ import { ProjectComponent } from './project.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatDialogModule } from '@angular/material/dialog';
 
-
-
-
-
 @NgModule({
   declarations: [ProjectDetailComponent, ProjectTeamComponent,
     ProjectComponent, CardLineChartComponent, CardBarChartComponent, CardPageVisitsComponent, CardSocialTrafficComponent, ProjectResComponent, ProjectDasboardComponent],
+
   imports: [
     CommonModule,
     RouterModule,
     MatTableModule,
     MatTabsModule,
     MatDialogModule,
-    NgxPaginationModule
+    NgxPaginationModule,
   ]
 
 })
