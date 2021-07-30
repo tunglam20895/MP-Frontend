fc741e550f556ba9723d5c99716977c7c9aafc8b
tunglam20895 <tunglam20895@gmail.com>
2021-07-30 14:08:09 +0700

    add pro

--- src/app/views/admin/project/add-project/add-project.component.ts
+++ src/app/views/admin/project/add-project/add-project.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { APIService } from 'src/app/api.service';
 import { NgForm } from '@angular/forms';
 import { Projects } from 'src/app/dto/projects';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-project',
@@ -12,11 +14,14 @@ export class AddProjectComponent implements OnInit {
 
   newPro: Projects = new Projects();
   pm: any;
-  constructor(private _service: APIService) {
+  constructor(private _service: APIService, private router: Router) {
     this._service.getListPM().subscribe(data => {
       this.pm = data;
       console.log(this.pm);
-    }, err => console.log(err))
+    }, err => {
+      console.log(err)
+
+    })
   }
 
   ngOnInit() {
@@ -27,8 +32,21 @@ export class AddProjectComponent implements OnInit {
     this._service.addProject(this.newPro).subscribe(data => {
       console.log(this.newPro);
       console.log(data);
-      console.log("Thêm thành công");
-    }, err => console.log("Lỗi" + err))
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Thêm dự án thành công',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      this.router.navigate(['admin/project'])
+    }, err => Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: 'Thêm dự án thất bại',
+      showConfirmButton: false,
+      timer: 1500
+    }))
   }
 
 }
